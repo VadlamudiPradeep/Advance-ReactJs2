6a51c9c1fb633b1d2f946c7ac0b5de179287523a
Vandlamudi Pradeep <vandlamudipradeepchoudhary@gmail.com>
2023-08-05 16:04:17 +0530

    making_the_expenses_visible_on_screen

--- Components/NewExpenses/ExpenseForm.js
+++ Components/NewExpenses/ExpenseForm.js
@@ -24,27 +24,31 @@ const ExpenseForm = () => {
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
-      date: enteredDate,
+      date:new Date(enteredDate) ,
     };
 
     console.log('expenseData ====>', expenseData);
    localStorage.setItem('expensesData', JSON.stringify(expenseData));
-
+   
+   setEnteredTitle('');
+   setEnteredAmount('');
+   setEnteredDate('');
+ 
   }
   return (
     <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' id='title' onChange={titleChangeHandler} />
+          <input type='text' id='title' value={enteredTitle}   onChange={titleChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' id='amount' min='0.01' step='0.01'   onChange={amountChangeHandler} />
+          <input type='number' id='amount' min='0.01' step='0.01' value={enteredAmount}   onChange={amountChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' id='date' min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler} />
+          <input type='date' id='date' min='2019-01-01' max='2022-12-31' value={enteredDate} onChange={dateChangeHandler} />
         </div>
       </div>
       <div className='new-expense__actions'>
