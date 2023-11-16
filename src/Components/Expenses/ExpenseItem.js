235d4070fd3f1c3544b154ac4d6b53c5271f88cc
Vandlamudi Pradeep <vandlamudipradeepchoudhary@gmail.com>
2023-11-16 23:23:20 +0530

    rendering_expenses_with_performance_optimisation

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,5 +1,3 @@
-// import React, { useState } from 'react';
-
 import React from 'react';
 
 import ExpenseDate from './ExpenseDate';
@@ -7,25 +5,20 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  // function clickHandler() {}
-//   const [title, setTitle] = useState(props.title);
-//   console.log('ExpenseItem evaluated by React');
-  
-//   const clickHandler = () => {
-//     setTitle('Updated!');
-//     console.log(title);
-//   };
+
 
   return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
-      {/* <button onClick={clickHandler}>Change Title</button> */}
-    </Card>
+    <li>
+      <Card className='expense-item'>
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
+        </div>
+      
+      </Card>
+    </li>
   );
-}
+};
 
 export default ExpenseItem;
